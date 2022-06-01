133126e9101a9511e37c733397ccc4e351fc9b6c
Mona Mohammed <mona00mona1997@gmail.com>
2022-06-01 11:35:35 +0300

    style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import feather from "./images/feather.png";
 
 class App extends React.Component {
   constructor(props) {
@@ -19,14 +20,13 @@ class App extends React.Component {
     let arr = this.state.tasks;
     arr.push(this.state.taskValue);
     this.setState({ tasks: arr });
-    const state = this.state.tasks[this.state.tasks.length-1];
+    const state = this.state.tasks[this.state.tasks.length - 1];
     // Put the object into storage
-    const localStorageList = JSON.parse(localStorage.getItem("toDoList"));
+    const localStorageList = JSON.parse(localStorage?.getItem("toDoList"))||[];
     localStorage.setItem(
       "toDoList",
       JSON.stringify([...localStorageList, state])
     );
-    
   };
 
   deleteItem = (i) => {
@@ -37,36 +37,39 @@ class App extends React.Component {
   };
 
   render() {
-    const list = JSON.parse(localStorage.getItem("toDoList"));
+    const list = JSON.parse(localStorage?.getItem("toDoList"))||[];
     return (
-      <section className="container">
-        <form className="head" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            placeholder="enter you task"
-            className="inpHead"
-            onChange={this.handleChange}
-          />
-          <button className="btnHead"> add </button>
-        </form>
+      <>
+        <img src={feather} alt="feather" />
 
-        <div className="lists">
-          {list.map((element, index) => (
-            <div className="listItem">
-              <p> {element}</p>
-              <div>
-                <button className="btnEdit">edit</button>
-                <button
-                  className="btnDelete"
-                  onClick={() => this.deleteItem(index)}
-                >
-                  delete
-                </button>
+        <section className="container">
+          <form className="head" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              placeholder="enter you task"
+              className="inpHead"
+              onChange={this.handleChange}
+            />
+            <button className="btnHead"> add </button>
+          </form>
+          <div className="lists">
+            {list.map((element, index) => (
+              <div className="listItem">
+                <p> {element}</p>
+                <div>
+                  <button className="btnEdit">edit</button>
+                  <button
+                    className="btnDelete"
+                    onClick={() => this.deleteItem(index)}
+                  >
+                    delete
+                  </button>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </section>
+            ))}
+          </div>
+        </section>
+      </>
     );
   }
 }
